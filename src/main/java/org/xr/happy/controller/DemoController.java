d65a3c4179748900d9b8d137fde8215d086a00cf
rkkPersonal <17635841699@163.com>
2021-02-02 20:24:10 +0800

    test

--- src/main/java/org/xr/happy/controller/DemoController.java
+++ src/main/java/org/xr/happy/controller/DemoController.java
@@ -1,14 +1,18 @@
 package org.xr.happy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
 
-    @GetMapping("/websocket")
-    public String index(){
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
 
+    @GetMapping("/websocket")
+    public String index() {
+        logger.info("websocket test ...");
         return "index";
     }
 }
