a6573a86fa0f56b2a5ef1daa6403db0183be1193
rkkPersonal <17635841699@163.com>
2021-02-03 22:59:25 +0800

    add websocket demo

--- src/main/java/org/xr/happy/config/WebSocketConfig.java
+++ src/main/java/org/xr/happy/config/WebSocketConfig.java
@@ -1,25 +1,15 @@
 package org.xr.happy.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-import org.xr.happy.common.utils.MySpringConfigurator;
 
-/**
- * @author Steven
- */
-@Component
+import javax.websocket.server.ServerEndpoint;
+
 @Configuration
 public class WebSocketConfig {
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
         return new ServerEndpointExporter();
-    }
-
-    @Bean
-    public MySpringConfigurator mySpringConfigurator() {
-        return new MySpringConfigurator();
     }
 }
