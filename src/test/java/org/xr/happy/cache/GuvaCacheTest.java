c25a4fd9ae78264f5e31ff77e6e29a743ee7a691
rkkPersonal <17635841699@163.com>
2021-02-16 17:04:51 +0800

    add websocket demo

---
+++ src/test/java/org/xr/happy/cache/GuvaCacheTest.java
@@ -0,0 +1,38 @@
+package org.xr.happy.cache;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+
+import java.util.concurrent.TimeUnit;
+
+public class GuvaCacheTest {
+
+    public static void main(String[] args) {
+
+        Cache cache = CacheBuilder.newBuilder().
+                maximumSize(10)
+                .expireAfterAccess(2, TimeUnit.SECONDS)
+                .removalListener(removalNotification -> {
+
+                    System.out.println("key is removed :"+removalNotification.getKey());
+
+                })
+                .build();
+
+
+        cache.put("steven","666");
+        Object u = cache.getIfPresent("steven");
+        System.out.println("steven:"+u);
+
+        try {
+            Thread .sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Object steven = cache.getIfPresent("steven");
+
+        System.out.println("steven:"+steven);
+    }
+}
