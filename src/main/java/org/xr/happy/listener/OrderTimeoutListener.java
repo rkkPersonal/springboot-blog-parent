0b8227960bcf32e8abb8f59a79a08fe52233921d
rkkPersonal <17635841699@163.com>
2021-02-16 19:28:57 +0800

    订单超时

--- src/main/java/org/xr/happy/listener/OrderTimeoutListener.java
+++ src/main/java/org/xr/happy/listener/OrderTimeoutListener.java
@@ -5,13 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 
+/**
+ * 除了可以用 keyspance 事件，
+ * @author Steven
+ */
 public class OrderTimeoutListener implements MessageListener {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderTimeoutListener.class);
 
-
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        logger.info("支付订单超时："+message);
+        logger.info("=========订单监听开始：===========" + message);
+
+        // todo 如果订单超时 ，则进行 修改订单状态。。。
+        if ("expired".equals(message.toString())) {
+            logger.info("订单已经超时，请重新下单。。。。");
+        }
     }
 }
