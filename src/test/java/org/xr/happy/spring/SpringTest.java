fad33e83aa3020d5032f7102aa79dc8fce68b48d
rkkPersonal <17635841699@163.com>
2021-02-16 19:07:01 +0800

    add websocket demo

--- src/test/java/org/xr/happy/spring/SpringTest.java
+++ src/test/java/org/xr/happy/spring/SpringTest.java
@@ -1,4 +1,6 @@
 package org.xr.happy.spring;
+
+import java.util.Arrays;
 import java.util.Date;
 
 
@@ -7,6 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,6 +22,7 @@ import org.xr.happy.Application;
 import org.xr.happy.model.User;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest(classes = Application.class)
 @RunWith(value = SpringJUnit4ClassRunner.class)
@@ -27,8 +35,46 @@ public class SpringTest {
 
 
     @Test
-    public void testRedisHash() throws Exception{
+    public void testRedisHash() throws Exception {
 
+
+    }
+
+    private void testRedisSubscriptPublish() throws Exception {
+        redisTemplate.execute(new RedisCallback<String>() {
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+
+                connection.subscribe((message, pattern) -> {
+                    System.out.println("收到:" + message.toString() + "," + Arrays.toString(pattern));
+
+                }, "__keyspace@0__:steven66".getBytes());
+                return null;
+            }
+        });
+
+
+        redisTemplate.opsForValue().set("steven66", "this is a test keyevent");
+
+        Thread.sleep(1000);
+        System.out.println("---------------开始删除key----------------");
+        //redisTemplate.delete("steven66");
+        redisTemplate.expire("steven66", 1, TimeUnit.SECONDS);
+
+
+        Thread.sleep(3000);
+
+        //收到:set,null
+        //---------------开始删除key----------------
+        //收到:del,null
+//---------------------------------------------------------------
+        // 收到:set,null
+        //---------------开始删除key----------------
+        //收到:expire,null key event 类型
+        //收到:expired,null key event 描述 代表已经过期了
+    }
+
+    private void testHash() throws Exception {
         User user = new User();
         user.setId(1);
         user.setUsername("steven");
@@ -41,14 +87,12 @@ public class SpringTest {
 
         ObjectMapper objectMapper = new ObjectMapper();
         Map map = objectMapper.readValue(user.toString(), Map.class);
-        redisTemplate.opsForHash().putAll(user.getId(),map);
+        redisTemplate.opsForHash().putAll(user.getId(), map);
 
 
         Object o = redisTemplate.opsForHash().get(user.getId(), user.getUsername());
 
         System.out.println(o);
 
-
     }
-
 }
