a088e484afe1352acd8a9dd29342658b659ebc4c
rkkPersonal <17635841699@163.com>
2021-04-24 21:20:09 +0800

    add generator order id by redis incre command

--- src/main/java/org/xr/happy/service/impl/OrderServiceImpl.java
+++ src/main/java/org/xr/happy/service/impl/OrderServiceImpl.java
@@ -25,12 +25,12 @@ public class OrderServiceImpl implements OrderGenerator {
     @Override
     public String generatorOrderId() {
         Long increment = redisTemplate.opsForValue().increment(ORDER_ID, -1);
-        String orderId = generator() + "-" + increment;
-        logger.info("订单号号是:{}",orderId);
+        String orderId = generatorPrefix()  + increment;
+        logger.info("订单号号是:{}", orderId);
         return orderId;
     }
 
-    private String generator() {
+    private String generatorPrefix() {
         LocalDateTime now = LocalDateTime.now();
         int year = now.getYear();
         int monthValue = now.getMonthValue();
@@ -38,7 +38,7 @@ public class OrderServiceImpl implements OrderGenerator {
         int hour = now.getHour();
         int minute = now.getMinute();
         int second = now.getSecond();
-        String id = year + monthValue + dayOfMonth + hour + minute + second + "";
+        String id = year + String.format("%02d", monthValue) + String.format("%02d", dayOfMonth) + String.format("%02d", hour) + String.format("%02d", minute) + String.format("%02d", second);
         return id;
     }
 }
